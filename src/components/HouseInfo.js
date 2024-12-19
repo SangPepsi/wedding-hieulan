f53a24bc4f4ebdf628816b5e85fc08117e21756a
Sang Luong Van <sanglv@officevnpay.com>
2024-12-19 11:07:13 +0700

    Add_redirects for Netlify routing

--- src/components/HouseInfo.js
+++ src/components/HouseInfo.js
@@ -4,19 +4,39 @@ import '../styles/HouseInfo.css'; // Import file CSS
 function HouseInfo() {
     return (
         <div className="house-info">
+            {/* Nhà Trai */}
             <div className="house house-groom">
                 <h4>Nhà Trai</h4>
                 <div className="parent">
-                    <p><span className="label">Bố:</span> <strong>Lương Văn Tâm</strong></p>
-                    <p><span className="label">Mẹ:</span> <strong>Phạm Thị Huê</strong></p>
+                    <p>
+                        <span className="label">Bố:</span>
+                        <strong>Lương Văn Tâm</strong>
+                    </p>
+                    <p>
+                        <span className="label">Mẹ:</span>
+                        <strong>Phạm Thị Huê</strong>
+                    </p>
+                </div>
+                <div className="quote">
+                    <em>"Gia đình là ngọn nguồn của sức mạnh và tình yêu thương."</em>
                 </div>
             </div>
 
+            {/* Nhà Gái */}
             <div className="house house-bride">
                 <h4>Nhà Gái</h4>
                 <div className="parent">
-                    <p><span className="label">Bố:</span> <strong>Lê Văn Cương</strong></p>
-                    <p><span className="label">Mẹ:</span> <strong>Đăng Thị Thinh</strong></p>
+                    <p>
+                        <span className="label">Bố:</span>
+                        <strong>Lê Văn Cương</strong>
+                    </p>
+                    <p>
+                        <span className="label">Mẹ:</span>
+                        <strong>Đăng Thị Thinh</strong>
+                    </p>
+                </div>
+                <div className="quote">
+                    <em>"Tình yêu bắt đầu từ gia đình và kết thúc trong hạnh phúc."</em>
                 </div>
             </div>
         </div>
