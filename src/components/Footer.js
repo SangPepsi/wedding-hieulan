2fb44f68728c8e8726d44c8cb2f8f5e74e5e6e27
Sang Luong Van <sanglv@officevnpay.com>
2024-12-19 14:53:12 +0700

    Add_redirects for Netlify routing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,13 @@ import React from 'react';
 import '../styles/Footer.css';
 
 function Footer() {
+    const currentYear = new Date().getFullYear();
+
     return (
         <footer className="footer">
-            <p>&copy; 2025 Đám Cưới Trung Hiếu & Lan Lê</p>
+            <div className="footer-content">
+                <p>&copy; {currentYear} SANGLV</p>
+            </div>
         </footer>
     );
 }
